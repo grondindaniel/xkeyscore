8965f6c3738994a1cc3522d90093f72caac98690
grondindaniel <daniel.grondin@ac-nantes.fr>
2020-08-15 23:54:36 +0200

    add filter list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository)
     {
-        return $this->render('home/index.html.twig', []);
+        $articles = $articleRepository->findAll();
+        return $this->render('home/index.html.twig', ['articles'=>$articles]);
     }
 }
